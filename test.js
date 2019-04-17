b7a0b6e01a72fe332c938ea17ec1425d4be96967
Bartosz Korczak <idbartosz@gmail.com>
2019-04-17 21:15:38 +0200

    Update test file

--- test.js
+++ test.js
@@ -2,22 +2,31 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect, bindActionCreators } from "react-redux";
 
-import Custom from "./Custom";
+import Custom, { badly } from "./Custom";
 import styles from "./Sassy.sass"; // Webpack will help us!
 import { getData } from "../actions/fetchData";
 
+const moduleGlobal = "bro";
+
+/**
+ * TODO: Waiting for React hooks to refactor this mess.
+ *
+ * @param {*} forgot Some param I've forgot
+ */
 @connect(
   state => ({ data: state.namedComponent.data }),
-  dispatch => bindActionCreators({ getData }, dispatch)
+  dispatch => bindActionCreators({ getMeta }, dispatch)
 )
+// Linting error: [ts] Experimental support for decorators is a feature
 export default class componentName extends Component {
   state = {
-    thisIsNuts: !!1 + " story",
-    tooMuchState: this.props.broFist
+    thisIsNuts: !!1 + " story" + ` ${moduleGlobal}`,
+    tooMuchState: this.props.broFist,
+    data: { data: [{}, {}, {}] }
   };
 
   static propTypes = {
-    importantObject: PropTypes.object,
+    importantObject: PropTypes.object.with[badly]['nested'].object.shape.types(),
     propertyOfMorty: PropTypes.any,
     broFist: PropTypes.element
   };
@@ -25,19 +34,25 @@ export default class componentName extends Component {
   static fetchData = store => store.dispatch(getData());
 
   componentDidMount = () => {
-    if (im(100) === "sure" && "drunk" !== wifeCall()) this.props.getData();
+    if (im(100) === "sure" && "drunk" !== wifeCall()) this.props.getMeta();
   };
 
   render() {
-    const custom = <Custom data={state.data} />,
-      fancy = <p className={propablyNot}> But will it work?</p>;
+    const { data: { data } } = state;
+    const { hidden: probablyNot } = styles
+    // Indeed there's no comma at line 42. JS and universe doesn't care, k?
+    const custom = <Custom data={data} />,
+      fancy = <p className={probablyNot}> But will it work?</p>;
     return (
       <>
+        {/* <- React.Fragment, ofc you knew... */}
         <div className={weDontNeedThisFancyDiv}>
           {custom || fancy}
+          {/* no render props, no inversion of control === user definitely not pro */}
           {this.props.children}
         </div>
       </>
     );
   }
 }
+// New line, take a break if you read all those comments 🥕
